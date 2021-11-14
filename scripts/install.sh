3d6f7d7b7258636d64d5efe9e6373c5ec1e2cfdf
Dmitry <dbulgak@gmail.com>
2021-11-14 19:19:21 +0300

    installation script update

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,13 +3,10 @@
 set -e
 
 # install dependencies
-yum -y install redis supervisord
+yum -y install redis supervisor
 
-systemctl enable supervisord
-systemctl start supervisord
-
-systemctl enable redis
-systemctl start redis
+systemctl enable supervisord && systemctl start supervisord
+systemctl enable redis && systemctl start redis
 
 # install supervisord configuration
 cp ../init/cachedproxy.ini /etc/supervisord.d/
